6587d3ba4db23ffaa830790d1f68e125eef36904
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2021-01-17 13:16:50 -0300

    agua style

--- src/components/AguaHome.js
+++ src/components/AguaHome.js
@@ -1,10 +1,11 @@
 import React,{Component} from 'react';
 import {View,TouchableHighlight,Text} from 'react-native'
+import {agua} from '../styles/Agua'
 
 class AguaHome extends Component {
     render() {
         return(
-            <View style={{flex: 1,backgroundColor: '#3EDDE8'}}>
+            <View style={agua.container}>
                 <TouchableHighlight onPress={()=>this.props.navigation.navigate("Homepage")}>
                     <Text>Voltar</Text>
                 </TouchableHighlight>
--- src/styles/Agua.js
+++ src/styles/Agua.js
@@ -2,7 +2,10 @@ import {StyleSheet} from 'react-native'
 
 const agua = StyleSheet.create({
     container:{
-        
+        flex: 1,
+        backgroundColor: '#3EDDE8',
+        justifyContent: 'center',
+        alignItems: 'center'
     }
 })
 
