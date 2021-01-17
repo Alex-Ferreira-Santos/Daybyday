408d93328b2d1c881f628ec93c4d6144801d4165
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2021-01-17 10:36:28 -0300

    layout inicial preparado

--- App.js
+++ App.js
@@ -1,114 +1,68 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React,{Component} from 'react';
+import {Text, View, TouchableHighlight,Image} from 'react-native'
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import {styles} from './src/styles/index'
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import copoDeagua from './src/img/copo-de-agua.png'
+import brocolis from './src/img/brocolis.png'
+import peso from './src/img/peso.png'
+import lista from './src/img/lista-de-tarefas.png'
+import lua from './src/img/lua-crescente.png'
+import config from './src/img/configuracao.png'
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+class App extends Component{
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Day by day</Text>
+        <Text style={styles.subtitle}>Viva mais saudável a cada dia</Text>
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+        <TouchableHighlight style={[styles.touchable,styles.agua]}>
+          <View style={styles.Buttoncontainer}>
+            <Image source={copoDeagua} style={styles.img}/>
+            <Text style={{fontSize: 20}}>Lembrete de água</Text>
+          </View> 
+        </TouchableHighlight>
 
-export default App;
+        <TouchableHighlight style={[styles.touchable,styles.alimentacao]}>
+          <View style={styles.Buttoncontainer}>
+            <Image source={brocolis} style={styles.img}/>
+            <Text style={{fontSize: 20}}>Alimentação</Text>
+          </View> 
+        </TouchableHighlight>
+
+        <TouchableHighlight style={[styles.touchable,styles.peso]}>
+          <View style={styles.Buttoncontainer}>
+            <Image source={peso} style={styles.img}/>
+            <Text style={{fontSize: 20}}>Peso</Text>
+          </View> 
+        </TouchableHighlight>
+
+        <TouchableHighlight style={[styles.touchable,styles.lista]}>
+          <View style={styles.Buttoncontainer}>
+            <Image source={lista} style={styles.img}/>
+            <Text style={{fontSize: 20}}>Lista de tarefas</Text>
+          </View> 
+        </TouchableHighlight>
+
+        <TouchableHighlight style={[styles.touchable,styles.dormir]}>
+          <View style={styles.Buttoncontainer}>
+            <Image source={lua} style={styles.img}/>
+            <Text style={{fontSize: 20,color: 'white'}}>Alimentação</Text>
+          </View> 
+        </TouchableHighlight>
+
+        <TouchableHighlight style={[styles.touchable,styles.extra]}>
+          <View style={styles.Buttoncontainer}>
+            <Image source={config} style={styles.img}/>
+            <Text style={{fontSize: 20,color: 'white'}}>Extra</Text>
+          </View> 
+        </TouchableHighlight>
+
+      </View>
+    )
+  }
+}
+
+
+export default App
---
+++ src/styles/index.js
@@ -0,0 +1,55 @@
+import {StyleSheet} from 'react-native'
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+        backgroundColor: 'lightblue',
+    },
+    title:{
+        fontSize: 40,
+    },
+    subtitle:{
+        fontSize: 20,
+    },
+    touchable:{
+        width:'90%',
+        borderRadius: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingVertical: 5,
+        margin: 5,
+    },
+    agua:{
+        backgroundColor: '#3EDDE8',
+    },
+    Buttoncontainer:{
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'row'
+    },
+    img:{
+        width:50,
+        height:50,
+        marginRight: 5,
+    },
+    alimentacao:{
+        backgroundColor: '#59A73D',   
+    },
+    peso:{
+        backgroundColor: '#DFCD2A'
+    },
+    lista:{
+        backgroundColor: '#B94B4B'
+    },
+    dormir:{
+        backgroundColor: '#483FB1'
+    },
+    extra:{
+        backgroundColor: '#9C359E'
+    }
+})
+
+export {styles}
