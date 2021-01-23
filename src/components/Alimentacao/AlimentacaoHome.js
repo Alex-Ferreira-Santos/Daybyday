6cb817691386de7163bbeb53bd85b7b10b91d4d2
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2021-01-23 14:18:56 -0300

    alimentação home

--- src/components/Alimentacao/AlimentacaoHome.js
+++ src/components/Alimentacao/AlimentacaoHome.js
@@ -6,12 +6,12 @@ import {alimentacaoHome} from '../../styles/Alimentacao'
 class AlimentacaoHome extends Component {
     render(){
         return (
-            <View>
-                <Text>Alimentação</Text>
-                <Text>Está sessão mostrará para você algumas dietas e receitas para que  possa manter uma alimentação  mais saúdavel lembrando que consultar um nutricionista é recomendado </Text>
-                <Image source={brocolis}/>
-                <TouchableHighlight>
-                    <Text>Próximo</Text>
+            <View style={alimentacaoHome.container}>
+                <Text style={alimentacaoHome.title}>Alimentação</Text>
+                <Text style={alimentacaoHome.text}>Está sessão mostrará para você algumas dietas e receitas para que possa manter uma alimentação  mais saúdavel lembrando que consultar um nutricionista é recomendado </Text>
+                <Image source={brocolis} style={alimentacaoHome.brocolis}/>
+                <TouchableHighlight style={alimentacaoHome.button} underlayColor='#1D470E' onPress={()=>this.props.navigation.navigate('')}>
+                    <Text style={alimentacaoHome.buttonText}>Próximo</Text>
                 </TouchableHighlight>
             </View>
         )
--- src/styles/Alimentacao.js
+++ src/styles/Alimentacao.js
@@ -6,7 +6,34 @@ const alimentacaoHome = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#59A73D'
+    },
+    title:{
+        fontSize: 30,
+        marginBottom: 20,
+    },
+    text:{
+        fontSize: 20,
+        textAlign: 'center',
+        width: '90%',
+        marginBottom: 20,
+    },
+    brocolis:{
+        width: 171,
+        height: 171,
+        marginBottom: 20,
+    },
+    button:{
+        backgroundColor: '#21560E',
+        width: '50%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 20,
+        paddingVertical: 3,
+    },
+    buttonText:{
+        fontSize: 20,
+        color: 'white'
     }
 })
 
-export default Alimentacao
+export {alimentacaoHome}
