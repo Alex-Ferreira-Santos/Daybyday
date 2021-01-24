1dfa2c688ae2bb4b57f90b12ef998db0993c70bb
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2021-01-24 10:18:58 -0300

    alimentação main

--- src/components/Alimentacao/AlimentacaoMain.js
+++ src/components/Alimentacao/AlimentacaoMain.js
@@ -1,17 +1,42 @@
 import React,{Component} from 'react';
-import {View,Text,TouchableHighlight} from 'react-native'
+import {View,Text,TouchableHighlight,ScrollView} from 'react-native'
 import {alimentacaoMain} from '../../styles/Alimentacao'
 
 class AlimentacaoMain extends Component{
-    render(){
-        return (
+    constructor(props){
+        super(props)
+        this.state = {
+            lastColor:''
+        }
+        this.corAleatoria = this.corAleatoria.bind(this)
+    }
+
+    corAleatoria(){
+        const cor = ['#BEF394','#88A563','#A1E150','#5B8B1D','#C3F680']
+        let corSelecionada = cor[Math.floor(Math.random() * cor.length)]
+        if(this.state.lastColor === corSelecionada){
+            corSelecionada = cor[Math.floor(Math.random() * cor.length)]
+        }
+        this.state.lastColor = corSelecionada
+        return corSelecionada
+    }
+    render(){ 
+        return ( 
             <View style={alimentacaoMain.container}>
                 <Text style={alimentacaoMain.title}>Alimentação</Text>
                 <Text style={alimentacaoMain.text}>Dietas disponíveis</Text>
-                <View style={alimentacaoMain.dietas}>
-
-                </View>
-                <TouchableHighlight style={alimentacaoMain.button}>
+                <ScrollView style={alimentacaoMain.dietas}>
+                    <TouchableHighlight style={[alimentacaoMain.dieta,{backgroundColor: this.corAleatoria()}]} underlayColor='#B6E98F' onPress={()=>{}}>
+                        <Text style={alimentacaoMain.dietaName}>Dieta da batata</Text>
+                    </TouchableHighlight>
+                    <TouchableHighlight style={[alimentacaoMain.dieta,{backgroundColor: this.corAleatoria()}]} underlayColor='#B6E98F' onPress={()=>{}}>
+                        <Text style={alimentacaoMain.dietaName}>Dieta da batata</Text>
+                    </TouchableHighlight> 
+                    <TouchableHighlight style={[alimentacaoMain.dieta,{backgroundColor: this.corAleatoria()}]} underlayColor='#B6E98F' onPress={()=>{}}>
+                        <Text style={alimentacaoMain.dietaName}>Dieta da batata</Text>
+                    </TouchableHighlight>
+                </ScrollView>
+                <TouchableHighlight style={alimentacaoMain.button} underlayColor='#1D470E' onPress={()=>{}}>
                     <Text style={alimentacaoMain.buttonText}>Inserir</Text>
                 </TouchableHighlight>
             </View>
--- src/styles/Alimentacao.js
+++ src/styles/Alimentacao.js
@@ -42,6 +42,44 @@ const alimentacaoMain = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#59A73D'
+    },
+    title:{
+        fontSize: 30,
+        marginVertical: 30,
+    },
+    text:{
+        fontSize: 20,
+        marginBottom: 20
+    },
+    dietas:{
+        backgroundColor: 'white',
+        width: '80%',
+        maxHeight: 300,
+        borderWidth: 1
+    },
+    button:{
+        backgroundColor: '#21560E',
+        width: '50%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 20,
+        paddingVertical: 3,
+        marginTop: 20
+    },
+    buttonText:{
+        fontSize: 20,
+        color: 'white'
+    },
+    dieta:{
+        width: '100%',
+        backgroundColor: '#BEF394',
+        height: 40,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderBottomWidth: 1,
+    },
+    dietaName:{
+        fontSize: 18
     }
 })
 
