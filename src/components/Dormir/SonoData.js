73dc630b2a259cb5dd3053584fbfb3f6034ee017
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2021-01-27 14:45:53 -0300

    sono data

--- src/components/Dormir/SonoData.js
+++ src/components/Dormir/SonoData.js
@@ -1,5 +1,6 @@
 import React,{Component} from 'react';
 import {View,Text,TouchableHighlight} from 'react-native'
+import Sono from '../../Database/Sono';
 import {sonoData} from '../../styles/Sono'
 import SonoPopup from './SonoPopup'
 
@@ -10,12 +11,17 @@ class SonoData extends Component{
             visible: false,
             enable: {},
             text: 'Desativar as notificações',
-            underlayColor: '#810000'
+            underlayColor: '#810000',
+            sono:[],
+            horas: '',
+            frase: ''
             
         }
+        this.sono = []
         this.closePopup = this.closePopup.bind(this)
         this.desable = this.desable.bind(this)
         this.enable = this.enable.bind(this)
+        this.select()
     }
 
     closePopup(){
@@ -34,14 +40,45 @@ class SonoData extends Component{
         this.setState({underlayColor: '#810000'})
     }
 
+    async select(){
+        const sono = new Sono
+        await sono.select().then( value => {
+            this.atribuiValor(value,this.sono)
+        })
+        this.setState({sono:this.sono})
+        this.CustomText()
+    }
+
+    atribuiValor(data,array){
+        array.push(data)
+        if(array.length > 1){
+          array.shift()
+        }
+    }
+
+    CustomText(){
+        this.setState({horas: this.state.sono[0][0].horasDormidas})
+        if(this.state.horas<6){
+            this.setState({frase:'horas de sono, não é o tempo ideal para dormir, você está dormindo bem pouco, tente ir para cama mais cedo se possivel para conseguir ajeitar seu sono, dormir pouco pode resultar em cansaço excessivo e vai te prejudicar'})
+        } else if(this.state.horas>=6 && this.state.horas<8){
+            this.setState({frase:'horas de sono, não é o tempo ideal para dormir mas está perto de estar ideal, tente tirar um cochilo de 1 hora para poder ter as horas arrumadas'})
+        }else if(this.state.horas>=8 && this.state.horas<10){
+            this.setState({frase:'horas de sono, parabéns, você está com o sono em dia'})
+        }else if(this.state.horas>=10 && this.state.horas<12){
+            this.setState({frase:'horas de sono, você está dormindo mais do que devia, tente colocar um despertador para acordar no horário certo para regular esse sono'})
+        }else if(this.state.horas>=12){
+            this.setState({frase:'horas de sono, você está dormindo bem acima do ideal, dormir essa quantidade pode resultar em sérios problemas de saúde'})
+        }
+    }
+
     render(){
         return(
             <View style={sonoData.container}>
-                <Text style={sonoData.title}>Na última noite você dormiu 7 horas</Text>
-                <Text style={sonoData.customText}>7 horas de sono não é o tempo ideal para dormir mas está perto de estar ideal, tente tirar um cochilo de 1 hora para poder ter as horas arrumadas</Text>
+                <Text style={sonoData.title}>Na última noite você dormiu {this.state.horas} horas</Text>
+                <Text style={sonoData.customText}>{this.state.horas} {this.state.frase}</Text>
                 <Text style={sonoData.alert}>Para te alertar a hora certa de dormir, uma notificação será enviada para você de acordo com a hora que você deseja dormir</Text>
 
-                <TouchableHighlight style={sonoData.changeButton} underlayColor='#00104A' onPress={()=>this.props.navigation.navigate('SonoForm')}>
+                <TouchableHighlight style={sonoData.changeButton} underlayColor='#00104A' onPress={()=>this.props.navigation.navigate('SonoForm',{edit:true})}>
                     <Text style={sonoData.ButtonText}>Trocar os horários</Text>
                 </TouchableHighlight>
 
