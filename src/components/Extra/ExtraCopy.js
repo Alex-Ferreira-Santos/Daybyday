c166bd2b01e40c922472fbce38ba09aff43d657a
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2021-07-18 11:56:51 -0300

    extra functions

--- src/components/Extra/ExtraCopy.js
+++ src/components/Extra/ExtraCopy.js
@@ -1,31 +1,29 @@
-import React,{Component} from 'react';
+import React from 'react';
 import {View,Text,TouchableHighlight,Linking} from 'react-native'
 import {extraCopy} from '../../styles/Extra'
 
-class ExtraCopy extends Component {
-    render(){
-        return (
-            <View style={extraCopy.container}>
-                <View style={extraCopy.main}>
-                    <Text style={extraCopy.title}>Copyrights</Text>
-                    <View style={extraCopy.links}>
-                        <Text style={extraCopy.copyrights}>Ícones feitos por <Text style={extraCopy.url} onPress={()=>Linking.openURL('https://www.flaticon.com/br/autores/flat-icons')}>Flat Icons</Text> from <Text style={extraCopy.url} onPress={()=>Linking.openURL('https://www.flaticon.com/br/')}>www.flaticon.com</Text></Text>
+function ExtraCopy({close}){
+    return (
+        <View style={extraCopy.container}>
+            <View style={extraCopy.main}>
+                <Text style={extraCopy.title}>Copyrights</Text>
+                <View style={extraCopy.links}>
+                    <Text style={extraCopy.copyrights}>Ícones feitos por <Text style={extraCopy.url} onPress={()=>Linking.openURL('https://www.flaticon.com/br/autores/flat-icons')}>Flat Icons</Text> from <Text style={extraCopy.url} onPress={()=>Linking.openURL('https://www.flaticon.com/br/')}>www.flaticon.com</Text></Text>
 
-                        <Text style={extraCopy.copyrights}>Ícones feitos por <Text style={extraCopy.url} onPress={()=>Linking.openURL('https://www.flaticon.com/br/autores/dinosoftlabs')}>DinosoftLabs</Text> from <Text style={extraCopy.url} onPress={()=>Linking.openURL('https://www.flaticon.com/br/')}>www.flaticon.com</Text></Text>
+                    <Text style={extraCopy.copyrights}>Ícones feitos por <Text style={extraCopy.url} onPress={()=>Linking.openURL('https://www.flaticon.com/br/autores/dinosoftlabs')}>DinosoftLabs</Text> from <Text style={extraCopy.url} onPress={()=>Linking.openURL('https://www.flaticon.com/br/')}>www.flaticon.com</Text></Text>
 
-                        <Text style={extraCopy.copyrights}>Ícones feitos por <Text style={extraCopy.url} onPress={()=>Linking.openURL('https://www.flaticon.com/br/autores/photo3idea-studio')}>photo3idea_studio</Text> from <Text style={extraCopy.url} onPress={()=>Linking.openURL('https://www.flaticon.com/br/')}>www.flaticon.com</Text></Text>
+                    <Text style={extraCopy.copyrights}>Ícones feitos por <Text style={extraCopy.url} onPress={()=>Linking.openURL('https://www.flaticon.com/br/autores/photo3idea-studio')}>photo3idea_studio</Text> from <Text style={extraCopy.url} onPress={()=>Linking.openURL('https://www.flaticon.com/br/')}>www.flaticon.com</Text></Text>
 
-                        <Text style={extraCopy.copyrights}>Ícones feitos por <Text style={extraCopy.url} onPress={()=>Linking.openURL('https://smashicons.com/')}>Smashicons</Text> from <Text style={extraCopy.url} onPress={()=>Linking.openURL('https://www.flaticon.com/br/')}>www.flaticon.com</Text></Text>
+                    <Text style={extraCopy.copyrights}>Ícones feitos por <Text style={extraCopy.url} onPress={()=>Linking.openURL('https://smashicons.com/')}>Smashicons</Text> from <Text style={extraCopy.url} onPress={()=>Linking.openURL('https://www.flaticon.com/br/')}>www.flaticon.com</Text></Text>
 
-                    </View>
-                    
-                    <TouchableHighlight style={extraCopy.button} underlayColor='#56BAB4' onPress={()=>{this.props.close()}}>
-                        <Text style={extraCopy.buttonText}>Voltar</Text>
-                    </TouchableHighlight>
-                </View> 
-            </View>
-        )
-    }
+                </View>
+                
+                <TouchableHighlight style={extraCopy.button} underlayColor='#56BAB4' onPress={()=>{close()}}>
+                    <Text style={extraCopy.buttonText}>Voltar</Text>
+                </TouchableHighlight>
+            </View> 
+        </View>
+    )
 }
 
 export default ExtraCopy
--- src/components/Extra/ExtraHome.js
+++ src/components/Extra/ExtraHome.js
@@ -1,43 +1,37 @@
-import React,{Component} from 'react';
+import React,{useState} from 'react';
 import {View,Text, TouchableHighlight,Linking} from 'react-native'
 import {extraHome} from '../../styles/Extra'
 import ExtraCopy from './ExtraCopy'
 
-class ExtraHome extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            copyVisible: false,
-        }
-        this.ClosePopup = this.ClosePopup.bind(this)
+function ExtraHome({close}){
+    const [copyVisible,setCopyVisible] = useState(false)
+
+    function ClosePopup(){
+        setCopyVisible(false)
     }
 
-    ClosePopup(){
-        this.setState({copyVisible: false})
-    }
+    return(
+        <View style={extraHome.container}>
+            <View style={extraHome.main}>
+                <TouchableHighlight style={extraHome.copyrightButton} underlayColor='#FF9E9E' onPress={()=>{setCopyVisible(true)}}>
+                    <Text style={extraHome.buttonText}>Copyrights</Text>
+                </TouchableHighlight>
 
-    render() {
-        return(
-            <View style={extraHome.container}>
-                <View style={extraHome.main}>
-                    <TouchableHighlight style={extraHome.copyrightButton} underlayColor='#FF9E9E' onPress={()=>{this.setState({copyVisible: true})}}>
-                        <Text style={extraHome.buttonText}>Copyrights</Text>
-                    </TouchableHighlight>
+                <Text style={extraHome.contato}>Contato</Text>
+                <Text style={extraHome.send}>Mande um email para:</Text>
+                <Text style={extraHome.email} onPress={()=>Linking.openURL('mailto:ricotefs@gmail.com')}>ricotefs@gmail.com</Text>
 
-                    <Text style={extraHome.contato}>Contato</Text>
-                    <Text style={extraHome.send}>Mande um email para:</Text>
-                    <Text style={extraHome.email} onPress={()=>Linking.openURL('mailto:ricotefs@gmail.com')}>ricotefs@gmail.com</Text>
-
-                    <TouchableHighlight style={extraHome.backButton} underlayColor='#5AA73F' onPress={()=>{
-                        this.props.close()
-                    }}>
-                        <Text style={extraHome.buttonText}>Voltar</Text>
-                    </TouchableHighlight>
-                </View>
-                {this.state.copyVisible && (<ExtraCopy close={this.ClosePopup}/>)}
+                <TouchableHighlight 
+                    style={extraHome.backButton} 
+                    underlayColor='#5AA73F' 
+                    onPress={()=>{close()}}
+                >
+                    <Text style={extraHome.buttonText}>Voltar</Text>
+                </TouchableHighlight>
             </View>
-        )
-    }
+            {copyVisible && (<ExtraCopy close={ClosePopup}/>)}
+        </View>
+    )
 }
 
 export default ExtraHome
